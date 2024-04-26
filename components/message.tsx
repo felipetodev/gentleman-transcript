e96948c3bd8f8ca522923c205a92fa4422fdde3b
felipetodev <fe.ossandon.u@gmail.com>
2024-04-26 16:19:09 -0400

    fix: improve copies

--- components/message.tsx
+++ components/message.tsx
@@ -1,23 +1,31 @@
 import Markdown from 'react-markdown'
 import { toast } from "sonner"
+import { cn } from '@/lib/utils'
 
 export function Message({ content }: { content?: string }) {
 
   function copyToClipboard() {
-    content && navigator.clipboard.writeText(content)
-    toast.success('Copied to clipboard')
+    if (content) {
+      navigator.clipboard.writeText(content)
+      toast.success('Copied to clipboard')
+
+    }
   }
 
   return (
     <div
       role="button"
       onClick={copyToClipboard}
-      className="p-4 bg-gray-800/30 rounded-lg cursor-copy h-[500px] border border-gray-500/50 overflow-y-auto"
+      className={cn("p-4 bg-gray-800/30 rounded-lg h-[500px] border border-gray-500/50 text-white overflow-y-auto", {
+        'cursor-not-allowed': !content,
+        'cursor-copy': content,
+      })}
     >
+      {!content && <span className='grid place-items-center size-full opacity-80'>
+        Not transcripted yet
+      </span>}
       {content && (
-        <Markdown
-          className="text-white"
-        >
+        <Markdown>
           {content}
         </Markdown>
       )}
