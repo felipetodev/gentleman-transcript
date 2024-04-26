808311340ca4b417137d4747f12a64629ce6db92
felipetodev <fe.ossandon.u@gmail.com>
2024-04-26 16:06:08 -0400

    feat: add init animation

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Form } from "@/components/ui/form";
 export default function Home() {
   return (
     <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-      <main className="max-w-4xl mx-auto space-y-2">
+      <main className="animate-fade-in-up max-w-4xl mx-auto space-y-2">
         <div className="flex items-center justify-center space-x-4">
           <h1 className='bg-gradient-to-t from-[#c7d2fe] to-[#8678f9] font-bold bg-clip-text text-4xl text-transparent'>
             Gentleman Youtube Transcripter
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
@@ -67,10 +67,21 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "fade-in-up": {
+          "0%": {
+            "opacity": "0",
+            "transform": "translateY(20px)",
+          },
+          "100%": {
+            "opacity": "1",
+            "transform": "translateY(0)"
+          }
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "fade-in-up": "fade-in-up 0.6s ease-in-out"
       },
     },
   },
