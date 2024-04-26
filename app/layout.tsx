fd6d02b26912b3cf31c53fc5035e7101896d4fc6
felipetodev <fe.ossandon.u@gmail.com>
2024-04-26 16:18:19 -0400

    feat: add repo link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner"
+import { GitIc } from "@/components/ui/icons";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <p className="absolute top-4 right-8">
+          <a
+            href="https://github.com/felipetodev/gentleman-transcript"
+            aria-label="View app repository on GitHub"
+            rel="noreferrer"
+            target="_blank"
+            className="flex items-center gap-2 text-secondary"
+          >
+            <GitIc className="size-5 " />
+            GitHub
+          </a>
+        </p>
         {children}
         <Toaster position="top-center" richColors />
       </body>
