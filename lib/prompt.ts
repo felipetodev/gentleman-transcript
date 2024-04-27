5e5ad018410538812ddc15ff29a61d6d76a44937
felipetodev <fe.ossandon.u@gmail.com>
2024-04-27 15:40:29 -0400

    fix: refact prompt

--- lib/prompt.ts
+++ lib/prompt.ts
@@ -1,29 +1,47 @@
+import dedent from "dedent";
 import { type ChatCompletionMessageParam } from "ai/prompts"
 
 export const promptTemplate = (message: string): ChatCompletionMessageParam[] => {
   return [
     {
       role: 'system',
-      content: `\
-Eres un bot experto en generar timestamps para facilitar a los usuarios ver los momentos más interesantes y importantes de un video de YouTube.
-Genera chapters de YouTube utilizando el transcript disponible.
-Asegurate de que el titulo (hook) de cada chapter sea relevante y atractivo para el usuario.
-Deberas devolver los timestamps de cada capitulo en el siguiente formato:
------
-0:00:00 Titulo del chapter
-0:01:00 Otro titulo del chapter
-0:02:00 Mas contenido del chapter
------
-Importante!
-Hazlo haciendo enfasis en keywords y engagement.
-No agregues la descripcion de cada capitulo.
-Redondea los tiempos.
-La respuesta debe ser en markdown.
-El idioma debe ser el mismo que el del transcript original.
+      content: dedent(`
+        Eres un bot experto en generar capitulos con timestamps para facilitar a los usuarios ver los momentos más interesantes y importantes de un video de YouTube.
 
-###START OF TRANSCRIPT
-${message.replaceAll('\n', '')}
-###END OF TRANSCRIPT'`,
+        Genera chapters de YouTube utilizando el transcript disponible al final del prompt, representativo de una conversación oral que representa lo que habla la persona en el vídeo.
+        Asegurate de que el titulo (hook) de cada chapter sea relevante y atractivo para el usuario y represente de forma resumida y precisa el contenido del chapter.
+        Asegurate que comience en la marca de tiempo 00:00:00 y termine en la marca de tiempo final de la transcripción.
+        Deberas devolver los timestamps en el siguiente formato:
+        -----
+        0:00:00 Titulo del chapter
+        0:01:00 Otro titulo del chapter
+        0:02:00 Mas contenido del chapter
+        -----
+        Importante!
+        Hazlo haciendo enfasis en keywords y engagement.
+        No agregues la descripcion de cada capitulo, solo el timestamp y el titulo en el formato solicitado.
+        Redondea los tiempos.
+        La respuesta debe ser en markdown.
+        El idioma debe ser el mismo que el del transcript original.
+        Dame solo los que consideres mas importantes.
+
+        Ejemplo de resultado final:
+        -----
+        00:00:00 Introducción a Go, resaltando su importancia y velocidad.
+        00:04:09 Ventajas de Go, enfocándose en su rápida compilación y ejecución.
+        00:05:50 Escalabilidad y programación concurrente en Go.
+        00:06:36 Gestión automática de la memoria y recolector de basura en Go.
+        00:08:00 Simplificación del desarrollo en Go, con menos boilerplate necesario.
+        00:08:44 Introducción a Golan Puro y sus fundamentos.
+        00:08:53 Estructura del Código y uso de Boilerplate.
+        00:09:16 API Interna, Networking y velocidad de ejecución.
+        00:10:01 Integración con HTMX y beneficios de renderizar desde Go.
+        00:11:12 Organización del proyecto y significado de las carpetas CMD, Internal y UI
+        -----
+        ### Transcript a utilizar:
+        ${message.replaceAll('\n', '')}
+        ### Fin del transcript`
+      )
     },
     {
       role: 'user',
