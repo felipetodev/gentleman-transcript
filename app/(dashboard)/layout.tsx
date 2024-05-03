c9f24521339d7ae5c0bf002e4bc52d1b8582dd02
felipetodev <fe.ossandon.u@gmail.com>
2024-05-02 21:57:34 -0400

    feat: add customer plan badge

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,19 @@
 import { HeaderDashboard } from "@/components/dashboard/header-dashboard";
+import { db } from "@/server/db";
+import { users } from "@/server/db/schema";
+import { auth } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const { userId } = auth()
+
+  const account = await db.query.users.findFirst({
+    where: eq(users.userId, userId!)
+  })
+
   return (
     <>
-      <HeaderDashboard />
+      <HeaderDashboard status={account?.status} />
       <main className="animate-fade-in-up flex flex-col justify-center mx-auto h-[calc(100%-48px)] max-w-4xl gap-y-3">
         {children}
       </main>
--- components/dashboard/header-dashboard.tsx
+++ components/dashboard/header-dashboard.tsx
@@ -1,17 +1,23 @@
 import { Moustache } from "@/components/ui/icons";
+import { Badge } from "@/components/ui/badge";
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
-export function HeaderDashboard() {
+export function HeaderDashboard({ status }: { status?: string | null }) {
   return (
     <header className="h-12 flex items-center justify-between px-6">
-      <Link
-        href="/"
-        className="flex items-center text-lg text-secondary font-bold tracking-tight"
-      >
-        <Moustache className="size-10 mr-2" />
-        Gentleman Transcript
-      </Link>
+      <div className="flex items-center space-x-2">
+        <Link
+          href="/"
+          className="flex items-center text-lg text-secondary font-bold tracking-tight"
+        >
+          <Moustache className="size-10 mr-2" />
+          Gentleman Transcript
+        </Link>
+        {status === "ACTIVE" && <Badge variant="gentleman">PRO</Badge>}
+        {status === "INACTIVE" && <Badge variant="secondary">Free</Badge>}
+        {status === "CANCELLED" && <Badge variant="secondary">Free</Badge>}
+      </div>
       <UserButton />
     </header>
   )
