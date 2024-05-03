e8a9f084903607fbf1e68889758fbc3beaa93d1b
felipetodev <fe.ossandon.u@gmail.com>
2024-05-02 21:57:51 -0400

    feat: protect api routes

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
   '/app(.*)',
-  '/api/transcript',
+  '/api/completion(.*)',
+  '/api/transcript(.*)',
 ]);
 
 export default clerkMiddleware((auth, req) => {
