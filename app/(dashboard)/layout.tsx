1daa6834a1bdc37154da162a9121ef8348466c6a
felipetodev <fe.ossandon.u@gmail.com>
2024-05-05 19:28:47 -0400

    fix: hide credits and tier badge if has no session

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -14,6 +14,7 @@ export default async function DashboardLayout({ children }: { children: React.Re
   return (
     <>
       <HeaderDashboard
+        user={Boolean(userId)}
         credits={account?.credits}
         hasPayment={Boolean(account?.stripeCustomerId)}
       />
--- components/dashboard/header-dashboard.tsx
+++ components/dashboard/header-dashboard.tsx
@@ -4,11 +4,12 @@ import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 type Props = {
+  user?: boolean,
   hasPayment?: boolean,
   credits?: number | null
 }
 
-export function HeaderDashboard({ credits = 0, hasPayment }: Props) {
+export function HeaderDashboard({ user = false, credits = 0, hasPayment }: Props) {
   return (
     <header className="h-12 flex items-center justify-between px-6">
       <div className="flex items-center space-x-2">
@@ -19,24 +20,32 @@ export function HeaderDashboard({ credits = 0, hasPayment }: Props) {
           <Moustache className="size-10 mr-2" />
           Gentleman Transcript
         </Link>
-        {hasPayment && credits && credits > 0 ? (
-          <Badge variant="gentleman">PRO</Badge>
-        ) : (
-          <Badge variant="secondary">Free</Badge>
-        )}
-        {!hasPayment && credits && (
-          <Badge variant="secondary">Free</Badge>
+        {user && (
+          <>
+            {hasPayment && credits && credits > 0 ? (
+              <Badge variant="gentleman">PRO</Badge>
+            ) : (
+              <Badge variant="secondary">Free</Badge>
+            )}
+            {!hasPayment && credits && (
+              <Badge variant="secondary">Free</Badge>
+            )}
+          </>
         )}
       </div>
       <div className="flex items-center space-x-2 text-[10px] text-secondary opacity-80">
-        {credits && credits > 0 ? (
-          <span>
-            {credits} Credit{credits > 1 ? "s" : ""}
-          </span>
-        ) : (
-          <span>
-            No credits left
-          </span>
+        {user && (
+          <>
+            {credits && credits > 0 ? (
+              <span>
+                {credits} Credit{credits > 1 ? "s" : ""}
+              </span>
+            ) : (
+              <span>
+                No credits left
+              </span>
+            )}
+          </>
         )}
         <UserButton />
       </div>
