4c6192a20cda3a505d1a86b5d35a592c14538388
felipetodev <fe.ossandon.u@gmail.com>
2024-05-16 13:51:23 -0400

    fix: try new prompt

--- lib/prompt.ts
+++ lib/prompt.ts
@@ -12,8 +12,9 @@ const transcriptTemplate = dedent(`
   The language must be the same as the original transcript.
   Ensure that the title (hook) of each chapter is relevant and engaging to the user and accurately represents the content of the chapter.
   Ensure that the first chapter commences precisely at timestamp 00:00:00 and concludes at the final timestamp of the transcription.
-  Your response only needs to include the timestamps and titles in the following format below. Eg.:
-  --------
+  Your response only needs to include the timestamps and titles in the following format below:
+
+  ##### START FORMAT EXAMPLE #####
   00:00:00 Introducción a Go, resaltando su importancia y velocidad.
   00:04:09 Ventajas de Go, enfocándose en su rápida compilación y ejecución.
   00:05:50 Escalabilidad y programación concurrente en Go.
@@ -24,7 +25,7 @@ const transcriptTemplate = dedent(`
   00:09:16 API Interna, Networking y velocidad de ejecución.
   00:10:01 Integración con HTMX y beneficios de renderizar desde Go.
   00:11:12 Organización del proyecto y significado de las carpetas CMD, Internal y UI.
-  --------
+  ##### END FORMAT EXAMPLE #####
 
   Important! Return your response in bullet points.
 
@@ -33,15 +34,14 @@ const transcriptTemplate = dedent(`
 const transcriptRefineTemplate = dedent(`\
   You are a helpful expert transcript bot that generates chapters with timestamps to help users find the most interesting and important moments in a YouTube video.
 
-  We have provided an existing answer: {existing_answer}
-  We have the opportunity to refine the existing answer
-  (only if needed) with some more context below.
+  We have provided an existing set of chapters with timestamps based on the original transcript: "{existing_answer}".
+  We have the opportunity to continue giving more chapters based on the new context provided below.
   --------
   {text}
   --------
   Given the new context above, please refine the existing answer or provide a new one.
-  Make sure to maintain the timestamps in the format provided below. For example:
-  --------
+  Make sure to maintain the timestamps in the format provided below:
+  ##### START FORMAT EXAMPLE #####
   00:00:00 Introducción a Go, resaltando su importancia y velocidad.
   00:04:09 Ventajas de Go, enfocándose en su rápida compilación y ejecución.
   00:05:50 Escalabilidad y programación concurrente en Go.
@@ -52,17 +52,19 @@ const transcriptRefineTemplate = dedent(`\
   00:09:16 API Interna, Networking y velocidad de ejecución.
   00:10:01 Integración con HTMX y beneficios de renderizar desde Go.
   00:11:12 Organización del proyecto y significado de las carpetas CMD, Internal y UI.
-  --------
+  ##### END FORMAT EXAMPLE #####
 
   Important!
   Emphasize keywords and engagement.
+  Do not provide timestamps that are earlier than the existing ones.
   Do not add the description of each chapter, only the timestamp and the title in the requested format.
+  Please do not repeat existing chapters or timestamps from: "{existing_answer}"
   Round the times.
   The language must be the same as the original transcript.
   Give me only the most important ones.
   Return your response in bullet points.
 
-  REFINED CHAPTERS AND TIMESTAMPS:`);
+  NEW CHAPTERS AND TIMESTAMPS:`);
 
 export const TRANSCRIPT_PROMPT = PromptTemplate.fromTemplate(
   transcriptTemplate
